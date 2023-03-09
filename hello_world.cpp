b72a1ba970449b8ad4f8e644b88f5628b02636ee
Vavik16 <slaba2016@yandex.ru>
2023-03-09 05:03:11 -0500

    changed hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
-using namespace std;
 int main() {
-    string name;
-    getline(std::cin, name);
-    cout << "Hello world from @" << name << endl;
+    std::string name;
+    std::getline(std::cin, name);
+    std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
